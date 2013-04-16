6a515462e461a3ea86a6e1853286ac168e1df2b7
Paul Park <paul@Paul-MacBookAir.(none)>
2013-04-16 13:43:07 -0400

    Updated Google Games Code

--- GoogleGames/ProfessorNormal.py
+++ GoogleGames/ProfessorNormal.py
@@ -1,14 +1,15 @@
 f = open('input')
+fw = open('output', 'w')
 
 def getNeighborsCount(cI, cJ, mat, maxM, maxN):
 	count = 0
-	if (cI-1 >= 0 and mat[cI-1,cJ] >= 0):
+	if (cI-1 >= 0 and mat[cI-1][cJ] >= 0):
 		count+=1
-	if (cI+1 < maxM and mat[cI+1,cJ] >= 0):
+	if (cI+1 < maxM and mat[cI+1][cJ] >= 0):
 		count+=1
-	if (cJ-1 >= 0 and mat[cI,cJ-1] >= 0):
+	if (cJ-1 >= 0 and mat[cI][cJ-1] >= 0):
 		count+=1
-	if (cJ+1 < maxN and mat[cI,cJ+1] >= 0):
+	if (cJ+1 < maxN and mat[cI][cJ+1] >= 0):
 		count+=1
 	
 	return count
@@ -16,18 +17,18 @@ def getNeighborsCount(cI, cJ, mat, maxM, maxN):
 def getNeighbors(cI, cJ, mat, maxM, maxN):
 	toRet = []
 	count = 0
-	if (cI-1 >= 0 and mat[cI-1,cJ] >= 0):
+	if (cI-1 >= 0 and mat[cI-1][cJ] >= 0):
                 toRet.append((cI-1,cJ))
 		count+=1
         print maxM
-	if (cI+1 < maxM and mat[cI+1,cJ] >= 0):
+	if (cI+1 < maxM and mat[cI+1][cJ] >= 0):
                 print count
 		toRet.append((cI+1,cJ))
 		count+=1
-        if (cJ-1 >= 0 and mat[cI,cJ-1] >= 0):
+        if (cJ-1 >= 0 and mat[cI][cJ-1] >= 0):
                 toRet.append((cI, cJ-1))
 		count+=1
-        if (cJ+1 < maxN and mat[cI,cJ+1] >= 0):
+        if (cJ+1 < maxN and mat[cI][cJ+1] >= 0):
                 toRet.append((cI, cJ+1))
 		count+=1
 
@@ -53,12 +54,15 @@ for x in range(0, len(numTests)) :
 	b = []
 
 	for l in range(M):
-		b.append([])
+		temp = []
+		for k in range(N):
+			temp.append(0)
+		b.append(temp)
 	
 	for y in range(M):
 		cols = f.readline()		
 		colsSplit = cols.split()
-		a[y] = append(colsSplit)
+		a[y] = colsSplit
 	
 	numExchanges = 0
 	numChildren = M*N
@@ -66,9 +70,9 @@ for x in range(0, len(numTests)) :
 	for t in range(10000) :
 		for i in range(M):
 			for j in range(N):
-				if (a[i,j] < 12 or getNeighborsCount(i, j, a, M, N) == 0):
+				if (a[i][j] < 12 or getNeighborsCount(i, j, a, M, N) == 0):
 					# Dis guy he out
-					a[i,j] = -1
+					a[i][j] = -1
 					numChildren-= 1
 		if (numChildren == 0):
 			break
@@ -77,15 +81,15 @@ for x in range(0, len(numTests)) :
 			for j in range(N):
 				numToGive = 12/getNeighborsCount(i, j, a, M, N)
 				neighborsCoordinate = getNeighbors(i, j, a, M, N)
-				b[i,j] -= 12
-				for z in range(neighborsCoordinate):
-					b[neighborsCoordinate[z][0],neighborsCoordinate[z][1]] += numToGive
+				b[i][j] -= 12
+				for z in range(len(neighborsCoordinate)):
+					b[neighborsCoordinate[z][0]][neighborsCoordinate[z][1]] += numToGive
 		numExchanges+= 1
 
 	if (numExchanges < 10000):
-		f.write('Case #' + str(M) + ':  ' + str(numExchanges) + ' turns\n')
+		fw.write('Case #' + str(M) + ':  ' + str(numExchanges) + ' turns\n')
 	else:
-		f.write('Case #' + str(M) + ':  ' + str(numChildren) + ' children will play forever') 
+		fw.write('Case #' + str(M) + ':  ' + str(numChildren) + ' children will play forever') 
 										
 
 
